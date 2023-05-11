a9147c09248566da69b91835e152543b5722c179
ag223 <126533276+ag223@users.noreply.github.com>
2023-05-10 20:49:50 -0500

    Ag/initial rev (#1)
    * hello world
    
    * Make sound
    
    ---------
    
    Co-authored-by: Asher Gunn <ashergunn@macbook-air-334.ads.northwestern.edu>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <driver/ledc.h>
+
+int channel = 0;
+int resolution = 8;
+double notes[] = {293.66/2, 392, 440, 587.33};
+double noteDurations[] = {300, 100, 100, 100};
+bool soundPlayed = false;
 
 void setup() {
-  // put your setup code here, to run once:
+  ledcAttachPin(23, channel);
+  ledcSetup(channel, 392, resolution);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!soundPlayed) {
+    ledcWrite(channel, 127);
+    for (int i = 0; i < 1000; i++) {
+      ledcWriteTone(channel, notes[i]);
+      delay(noteDurations[i] + 100);
+    }
+    ledcWrite(channel, 0);
+    soundPlayed = true;
+  }
+
 }
