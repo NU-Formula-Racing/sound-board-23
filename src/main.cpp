05b6bf92d3c9e8f3c18003e4a0f91b9f7c22c8d5
jesusm2024 <jesusmontero2024@u.northwestern.edu>
2023-05-27 14:06:03 -0500

    Revamped sound board code to work with throttle status.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,54 @@
 #include <Arduino.h>
 #include <driver/ledc.h>
+#include "soundBoard.h"
+#include "virtualTimer.h"
 
-int channel = 0;
-int resolution = 8;
-double notes[] = {293.66/2, 392, 440, 587.33};
-double noteDurations[] = {300, 100, 100, 100};
-bool soundPlayed = false;
+#define SERIAL_DEBUG
 
-void setup() {
-  ledcAttachPin(23, channel);
-  ledcSetup(channel, 392, resolution);
+#ifdef ARDUINO_ARCH_ESP32
+#include "esp_can.h"
+// The tx and rx pins are constructor arguments to ESPCan, which default to TX = 5, RX = 4
+ESPCAN can_bus{};
+#endif
+
+// Initialize timer to call functions at
+// specified frequencies.
+VirtualTimerGroup read_timer;
+
+// Initialize board.
+SoundBoard sound_board;
+
+// Read throttle status from CAN.
+CANSignal<SoundBoard::ETCState, 0, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> throttle_status_rx_signal{};
+CANRXMessage<1> rx_message{can_bus, sound_board.kCANID, throttle_status_rx_signal};
+
+/**
+ * @brief Function that calls the PlaySound
+ * function in the timer group.
+ */
+void ThrottleStatusCheck()
+{
+  sound_board.PlaySound(throttle_status_rx_signal);
 }
 
-void loop() {
-  if (!soundPlayed) {
-    ledcWrite(channel, 127);
-    for (int i = 0; i < 1000; i++) {
-      ledcWriteTone(channel, notes[i]);
-      delay(noteDurations[i] + 100);
-    }
-    ledcWrite(channel, 0);
-    soundPlayed = true;
-  }
+void setup()
+{
 
+#ifdef SERIAL_DEBUG
+  // Initialize serial output
+  Serial.begin(9600);
+#endif
+
+  // Initialize CAN bus.
+  can_bus.Initialize(ICAN::BaudRate::kBaud1M);
+
+  // Initialize our timer(s)
+  read_timer.AddTimer(100, ThrottleStatusCheck);
+}
+
+void loop()
+{
+  // Necessary to tick the CAN bus to get the throttle_status_rx_sign.
+  can_bus.Tick();
+  read_timer.Tick(millis());
 }
